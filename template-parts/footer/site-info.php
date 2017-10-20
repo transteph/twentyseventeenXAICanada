254e08ae2c176913b52b405a9f9fa9ce3945d5fa
Stephanie <stran40@myseneca.ca>
2017-10-20 16:06:39 -0400

    Removed float on front page entries. Removed original theme footer.

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,5 +10,4 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
 </div><!-- .site-info -->
