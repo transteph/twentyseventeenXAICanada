a429b2c8b7633ca8e6c8a36b774821da4bd49f01
Stephanie <stran40@myseneca.ca>
2017-10-22 18:35:25 -0400

    Removed wrapping around front-page top heading. Added padding to first main nav element to prevent overlapping logo.

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -10,7 +10,6 @@
 
 ?>
 <div class="site-branding">
-	<div class="wrap">
 
 		<?php the_custom_logo(); ?>
 
@@ -34,5 +33,4 @@
 		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
 	<?php endif; ?>
 
-	</div><!-- .wrap -->
 </div><!-- .site-branding -->
